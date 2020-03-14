5cb4e9841be88cf87c31342da65f6bcd15e14499
Bassam Helal <basshelal@gmail.com>
2020-03-14 02:30:32 +0000

    Begun work on automatic scrolling for dragging lists and doing so even when the user is not moving their finger by using RxJava

--- boardview/src/main/java/com/github/basshelal/boardview/BoardViewContainer.kt
+++ boardview/src/main/java/com/github/basshelal/boardview/BoardViewContainer.kt
@@ -11,9 +11,16 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.contains
+import androidx.recyclerview.widget.RecyclerView.NO_POSITION
 import com.github.basshelal.boardview.drag.DragShadow
 import com.github.basshelal.boardview.drag.ObservableDragBehavior
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.container_boardviewcontainer.view.*
+import java.util.concurrent.TimeUnit
 
 /**
  * The container that will contain a [BoardView] as well as the [DragShadow]s for dragging
@@ -40,6 +47,9 @@ class BoardViewContainer
 
     var draggingItemVH: BoardViewItemVH? = null
     var draggingBoardViewVH: BoardViewVH? = null
+
+    private val boardVHSwaps = HashMap<ViewHolderSwap<BoardViewVH>, Boolean>()
+    private val itemVHSwaps = HashMap<ViewHolderSwap<BoardViewItemVH>, Boolean>()
 
     init {
         View.inflate(context, R.layout.container_boardviewcontainer, this)
@@ -75,21 +85,66 @@ class BoardViewContainer
     private inline fun listDragShadow() {
         listDragShadow.dragBehavior.dragListener = object : ObservableDragBehavior.SimpleDragListener() {
 
+            val onNext = {
+                logE(now)
+                // Same shit in onUpdateLocation except this can run even when user doesn't
+                // update drag location (so they're holding still)
+                if (draggingBoardViewVH != null) {
+                    checkForScroll(touchPointF)
+                    findBoardViewHolderUnderRaw(touchPointF.x, touchPointF.y)?.also { newVH ->
+                        draggingBoardViewVH?.also { draggingVH ->
+                            swapViewHolders(draggingVH, newVH)
+                        }
+                    }
+                }
+            }
+
+            var disposable: Disposable? = null
+
             override fun onStartDrag(dragView: View) {
                 draggingBoardViewVH = findBoardViewHolderUnderRaw(touchPointF.x, touchPointF.y)
                 draggingBoardViewVH?.itemView?.setBackgroundColor(Color.MAGENTA)
+                disposable = Observable.interval(16L, TimeUnit.MILLISECONDS)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.computation())
+                        .subscribe { onNext() }
             }
 
             override fun onUpdateLocation(dragView: View, touchPoint: PointF) {
-                findBoardViewHolderUnderRaw(touchPoint.x, touchPoint.y)?.also { newVH ->
-                    if (newVH != draggingBoardViewVH) {
-                        draggingBoardViewVH?.itemView?.setBackgroundColor(Color.TRANSPARENT)
-                        newVH.itemView.setBackgroundColor(Color.CYAN)
-                        swapBoardViewHolders(draggingBoardViewVH!!, newVH)
-                        // TODO: 13-Mar-20 Do shit after the animation of swap is finished somehow!
+
+            }
+
+            fun swapViewHolders(oldVH: BoardViewVH, newVH: BoardViewVH) {
+                val swap = ViewHolderSwap(oldVH, newVH)
+                if (!boardVHSwaps.containsKey(swap)) boardVHSwaps[swap] = false
+                if (newVH != oldVH && boardVHSwaps[swap] == false) {
+                    swapBoardViewHolders(oldVH, newVH)
+                    doAfterFinishAnimation {
+                        boardVHSwaps[swap] = true
+                        boardVHSwaps.remove(swap)
                         draggingBoardViewVH = newVH
                     }
                 }
+            }
+
+            // Horizontal Scroll
+            fun checkForScroll(touchPoint: PointF) {
+                val scrollBy = 16
+                val width = boardView.globalVisibleRectF.width() / 8F
+                val leftBounds = boardView.globalVisibleRectF.also { it.right = width }
+                val rightBounds = boardView.globalVisibleRectF.also { it.left = it.width() - width }
+                when {
+                    leftBounds.contains(touchPoint) -> {
+                        boardView.scrollBy(-scrollBy, 0)
+                    }
+                    rightBounds.contains(touchPoint) -> {
+                        boardView.scrollBy(scrollBy, 0)
+                    }
+                }
+            }
+
+            override fun onReleaseDrag(dragView: View, touchPoint: PointF) {
+                disposable?.dispose()
             }
 
             override fun onEndDrag(dragView: View) {
@@ -153,13 +208,26 @@ class BoardViewContainer
     }
 
     fun swapBoardViewHolders(old: BoardViewVH, new: BoardViewVH) {
-        (boardView.adapter as? BoardAdapter)?.apply {
-            notifyItemChanged(old.adapterPosition)
-            notifyItemChanged(new.adapterPosition)
+        val from = old.adapterPosition
+        val to = new.adapterPosition
+        val boardAdapter = boardView.adapter as? BoardAdapter
+
+        if (from != NO_POSITION && to != NO_POSITION) {
+            logE("Swapping from $from to $to")
+            boardAdapter?.apply {
+                notifyItemMoved(from, to)
+                notifyItemMoved(to, from)
+            }
         }
+    }
+
+    inline fun doAfterFinishAnimation(crossinline block: (BoardView) -> Unit) {
+        boardView.itemAnimator?.isRunning { block(boardView) }
     }
 }
 
+// Represents a ViewHolder Swap which we can use to track if swaps are completed
+private data class ViewHolderSwap<VH : BaseViewHolder>(val old: VH, val new: VH)
 
 abstract class BoardContainerAdapter {
 
